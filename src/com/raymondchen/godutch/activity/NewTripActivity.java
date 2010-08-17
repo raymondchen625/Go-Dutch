d9d8ffa43f9a0d90572a24b6569bbabfc4f0f90f
Raymond Chen <raymondchen625@gmail.com>
2010-08-17 10:09:22 +0800

    修正了新建活动时没选择用户返回会崩溃的问题

--- src/com/raymondchen/godutch/activity/NewTripActivity.java
+++ src/com/raymondchen/godutch/activity/NewTripActivity.java
@@ -22,67 +22,81 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewTripActivity extends Activity {
-	private static final int SELECT_USER=1;
+	private static final int SELECT_USER = 1;
 	private Button selectUsersButton;
 	private TextView selectedUserListTextView;
 	private Button addTripButton;
 	private EditText newTripEditText;
-	private List<User> selectedUserList=new ArrayList<User>();
+	private List<User> selectedUserList = new ArrayList<User>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_trip);
-		selectUsersButton=(Button)findViewById(R.id.selectUsersButton);
-		selectedUserListTextView=(TextView)findViewById(R.id.selectedUserListTextView);
-		newTripEditText=(EditText)findViewById(R.id.newTripEditText);
-		addTripButton=(Button)findViewById(R.id.addTripButton);
+		selectUsersButton = (Button) findViewById(R.id.selectUsersButton);
+		selectedUserListTextView = (TextView) findViewById(R.id.selectedUserListTextView);
+		newTripEditText = (EditText) findViewById(R.id.newTripEditText);
+		addTripButton = (Button) findViewById(R.id.addTripButton);
 		selectUsersButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				startActivityForResult(new Intent(getApplicationContext(),SelectTripUsersActivity.class),SELECT_USER);
+				startActivityForResult(new Intent(getApplicationContext(),
+						SelectTripUsersActivity.class), SELECT_USER);
 			}
 		});
 		addTripButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				if (selectedUserList==null || selectedUserList.size()<=1) {
-					Toast.makeText(getApplicationContext(), getResources().getText(R.string.selectUsersPlease), Toast.LENGTH_SHORT).show();
-					return ;
+				if (selectedUserList == null || selectedUserList.size() <= 1) {
+					Toast.makeText(getApplicationContext(),
+							getResources().getText(R.string.selectUsersPlease),
+							Toast.LENGTH_SHORT).show();
+					return;
 				} else if (newTripEditText.getText().toString().equals("")) {
-					Toast.makeText(getApplicationContext(), getResources().getText(R.string.specifyTripNamePlease), Toast.LENGTH_SHORT).show();
-					return ;
+					Toast.makeText(
+							getApplicationContext(),
+							getResources().getText(
+									R.string.specifyTripNamePlease),
+							Toast.LENGTH_SHORT).show();
+					return;
 				}
-				Trip trip=new Trip();
+				Trip trip = new Trip();
 				trip.setName(newTripEditText.getText().toString());
 				trip.setMembers(selectedUserList);
 				DataService.addTrip(getApplicationContext(), trip);
-				Toast.makeText(getApplicationContext(), getResources().getText(R.string.addTripSucceeded), Toast.LENGTH_LONG).show();
+				Toast.makeText(getApplicationContext(),
+						getResources().getText(R.string.addTripSucceeded),
+						Toast.LENGTH_LONG).show();
 				finish();
 			}
-			
+
 		});
 	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		Bundle bundle=data.getBundleExtra(getPackageName());
-		long[] userIdArray=bundle.getLongArray(getPackageName());
-		refreshSelectedUserList(userIdArray);
-	}
-	
-	private void refreshSelectedUserList(long[] userIdArray) {
-		this.selectedUserList=new ArrayList<User>();
-		for (long userId : userIdArray) {
-			selectedUserList.add(DataService.getUserById(getApplicationContext(), userId));
+		if (resultCode == Activity.RESULT_OK) {
+			Bundle bundle = data.getBundleExtra(getPackageName());
+			long[] userIdArray = bundle.getLongArray(getPackageName());
+			refreshSelectedUserList(userIdArray);
 		}
-		String userListText="";
-		for (int i=0;i<selectedUserList.size();i++) {
-			userListText+=selectedUserList.get(i).getName();
-			if (i<selectedUserList.size()-1) {
-				userListText+=", ";
+	}
+
+	private void refreshSelectedUserList(long[] userIdArray) {
+		this.selectedUserList = new ArrayList<User>();
+		for (long userId : userIdArray) {
+			selectedUserList.add(DataService.getUserById(
+					getApplicationContext(), userId));
+		}
+		String userListText = "";
+		for (int i = 0; i < selectedUserList.size(); i++) {
+			userListText += selectedUserList.get(i).getName();
+			if (i < selectedUserList.size() - 1) {
+				userListText += ", ";
 			}
 		}
 		selectedUserListTextView.setText(userListText);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
